a5b0bb4c95e1348bc0fb9de02b2177c74027ccca
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-21 14:20:28 +0530

    [add] Shubham added UC1 program

---
+++ IndianStateCensusAnalyser/DTO/CensusDTO.cs
@@ -0,0 +1,52 @@
+﻿using IndianStateCensusAnalyser.POCO;
+using System;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int tin;
+        public string stateCode;
+        public long population;
+        public long area;
+        public long density;
+        public long housingUnits;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+
+        public CensusDTO(StateCodeDAO stateCodeDAO)
+        {
+            this.serialNumber = stateCodeDAO.serialNumber;
+            this.stateName = stateCodeDAO.stateName;
+            this.tin = stateCodeDAO.tin;
+            this.stateCode = stateCodeDAO.stateCode;
+        }
+
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.state = censusDataDAO.state;
+            this.population = censusDataDAO.population;
+            this.area = censusDataDAO.area;
+            this.density = censusDataDAO.density;
+        }
+
+        //public CensusDTO(USCensusDAO uSCensusDAO)
+        //{
+        //    this.stateCode = uSCensusDAO.stateId;
+        //    this.stateName = uSCensusDAO.stateName;
+        //    this.population = uSCensusDAO.population;
+        //    this.housingUnits = uSCensusDAO.houseUnits;]
+        //    this.totalArea = uSCensusDAO.totalArea;
+        //    this.waterArea = uSCensusDAO.waterArea;
+        //    this.landArea = uSCensusDAO.landArea;
+        //    this.populationDensity = uSCensusDAO.populationDensity;
+        //    this.housingDensity = uSCensusDAO.housingDensity;
+        //}
+    }
+}
---
+++ IndianStateCensusAnalyser/POCO/CensusDataDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    public class CensusDataDAO
+    {
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+
+        public CensusDataDAO(string state, string population, string area, string density)
+        {
+            this.state = state;
+            this.population = Convert.ToInt32(population);
+            this.area = Convert.ToInt32(area);
+            this.density = Convert.ToInt32(density);
+        }
+    }
+}
